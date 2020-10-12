9fc1b856b52659330a9afadd8a53a9771f952190
sandeepnetha <sandeepnetha.gajula@gmail.com>
2020-10-12 21:14:36 +0530

    variables

---
+++ variables.tf
@@ -0,0 +1,214 @@
+variable "region" {
+  type        = string
+  description = "AWS Region"
+  default     ="ohio"
+}
+variable "clustername"{
+  type    = string
+}
+variable "vpc_id" {
+  type        = string
+  default     = "vpc-0542fe1f797294397"
+  description = "VPC ID for the EKS cluster"
+}
+
+variable "subnet_ids" {
+  description = "A list of subnet IDs to launch the cluster in"
+  type        = list(string)
+  default     = ["subnet-0043fb7c5bddc8705","subnet-06e0e661239ffd6fb"]
+}
+
+variable "allowed_security_groups" {
+  type        = list(string)
+  default     = []
+  description = "List of Security Group IDs to be allowed to connect to the EKS cluster"
+}
+
+variable "allowed_cidr_blocks" {
+  type        = list(string)
+  default     = []
+  description = "List of CIDR blocks to be allowed to connect to the EKS cluster"
+}
+
+variable "workers_role_arns" {
+  type        = list(string)
+  description = "List of Role ARNs of the worker nodes"
+  default     = []
+}
+
+variable "workers_security_group_ids" {
+  type        = list(string)
+  description = "Security Group IDs of the worker nodes"
+  default     = []
+}
+
+variable "kubernetes_version" {
+  type        = string
+  default     = "1.15"
+  description = "Desired Kubernetes master version. If you do not specify a value, the latest available version is used"
+}
+
+variable "kubernetes_config_path" {
+  type        = string
+  default     = "~/.kube/config"
+  description = "Path to the kube config file. Defaults to `~/.kube/config`"
+}
+
+variable "kubernetes_load_config_file" {
+  type        = bool
+  default     = false
+  description = "Loads the default local config of ~/.kube/config for the provider, which is useful for resolving migration issues like `Error: configmaps \"aws-auth\" already exists`"
+}
+
+variable "oidc_provider_enabled" {
+  type        = bool
+  default     = false
+  description = "Create an IAM OIDC identity provider for the cluster, then you can create IAM roles to associate with a service account in the cluster, instead of using kiam or kube2iam. For more information, see https://docs.aws.amazon.com/eks/latest/userguide/enable-iam-roles-for-service-accounts.html"
+}
+
+variable "endpoint_private_access" {
+  type        = bool
+  default     = false
+  description = "Indicates whether or not the Amazon EKS private API server endpoint is enabled. Default to AWS EKS resource and it is false"
+}
+
+variable "endpoint_public_access" {
+  type        = bool
+  default     = true
+  description = "Indicates whether or not the Amazon EKS public API server endpoint is enabled. Default to AWS EKS resource and it is true"
+}
+
+variable "public_access_cidrs" {
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+  description = "Indicates which CIDR blocks can access the Amazon EKS public API server endpoint when enabled. EKS defaults this to a list with 0.0.0.0/0."
+}
+
+variable "enabled_cluster_log_types" {
+  type        = list(string)
+  default     = []
+  description = "A list of the desired control plane logging to enable. For more information, see https://docs.aws.amazon.com/en_us/eks/latest/userguide/control-plane-logs.html. Possible values [`api`, `audit`, `authenticator`, `controllerManager`, `scheduler`]"
+}
+
+variable "cluster_log_retention_period" {
+  type        = number
+  default     = 0
+  description = "Number of days to retain cluster logs. Requires `enabled_cluster_log_types` to be set. See https://docs.aws.amazon.com/en_us/eks/latest/userguide/control-plane-logs.html."
+}
+
+variable "apply_config_map_aws_auth" {
+  type        = bool
+  default     = true
+  description = "Whether to apply the ConfigMap to allow worker nodes to join the EKS cluster and allow additional users, accounts and roles to acces the cluster"
+}
+
+variable "map_additional_aws_accounts" {
+  description = "Additional AWS account numbers to add to `config-map-aws-auth` ConfigMap"
+  type        = list(string)
+  default     = []
+}
+
+variable "map_additional_iam_roles" {
+  description = "Additional IAM roles to add to `config-map-aws-auth` ConfigMap"
+
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = []
+}
+
+variable "map_additional_iam_users" {
+  description = "Additional IAM users to add to `config-map-aws-auth` ConfigMap"
+
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = []
+}
+
+variable "local_exec_interpreter" {
+  type        = list(string)
+  default     = ["/bin/sh", "-c"]
+  description = "shell to use for local_exec"
+}
+
+variable "wait_for_cluster_command" {
+  type        = string
+  default     = "curl --silent --fail --retry 60 --retry-delay 5 --retry-connrefused --insecure --output /dev/null $ENDPOINT/healthz"
+  description = "`local-exec` command to execute to determine if the EKS cluster is healthy. Cluster endpoint are available as environment variable `ENDPOINT`"
+}
+
+variable "kubernetes_config_map_ignore_role_changes" {
+  type        = bool
+  default     = true
+  description = "Set to `true` to ignore IAM role changes in the Kubernetes Auth ConfigMap"
+}
+
+variable "cluster_encryption_config_enabled" {
+  type        = bool
+  default     = false
+  description = "Set to `true` to enable Cluster Encryption Configuration"
+}
+
+variable "cluster_encryption_config_kms_key_id" {
+  type        = string
+  default     = ""
+  description = "Specify KMS Key Id ARN to use for cluster encryption config"
+}
+
+variable "cluster_encryption_config_kms_key_enable_key_rotation" {
+  type        = bool
+  default     = true
+  description = "Cluster Encryption Config KMS Key Resource argument - enable kms key rotation"
+}
+
+variable "cluster_encryption_config_kms_key_deletion_window_in_days" {
+  type        = number
+  default     = 10
+  description = "Cluster Encryption Config KMS Key Resource argument - key deletion windows in days post destruction"
+}
+
+variable "cluster_encryption_config_kms_key_policy" {
+  type        = string
+  default     = null
+  description = "Cluster Encryption Config KMS Key Resource argument - key policy"
+}
+
+variable "cluster_encryption_config_resources" {
+  type        = list
+  default     = ["secrets"]
+  description = "Cluster Encryption Config Resources to encrypt, e.g. ['secrets']"
+}
+variable "health_check_type" {
+  type        = string
+  description = "Controls how health checking is done. Valid values are `EC2` or `ELB`"
+  default     = "EC2"
+}
+variable "max_size" {
+  default     = 3
+  description = "The maximum size of the AutoScaling Group"
+}
+
+variable "min_size" {
+  default     = 2
+  description = "The minimum size of the AutoScaling Group"
+}
+variable "wait_for_capacity_timeout" {
+  type        = string
+  description = "A maximum duration that Terraform should wait for ASG instances to be healthy before timing out. Setting this to '0' causes Terraform to skip all Capacity Waiting behavior"
+  default     = "10m"
+}
+variable "instance_type" {
+  type        = list(string)
+  description = "Instance type to launch"
+}
+variable "ec2_ssh_key" {
+  type = string
+  default = "null"
+}
